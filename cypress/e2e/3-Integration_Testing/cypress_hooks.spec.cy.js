f6dcb6c13e5fd7f3288899cd11c024c731d188d2
mmccorm4 <mmccorm4@jaguarlandrover.com>
2023-02-02 11:04:02 +0000

    chore(DEVX-XXX): COMMIT TITLE

--- cypress/e2e/3-Integration_Testing/cypress_hooks.spec.cy.js
+++ cypress/e2e/3-Integration_Testing/cypress_hooks.spec.cy.js
@@ -5,29 +5,29 @@
 //after : runs one time after all the tests are finsihed
 //aftrEach : runs after each individual test
 
-    describe('Cypress Hooks', () => {
-      before(() =>{
-          cy.request('https://api.spacexdata.com/v3/missions').its('body').should('have.length', 10)
-      })
-      //before / beforeAll is run at the beginning of the test run
-      beforeEach(() =>{ 
-          cy.visit('/')
-      })
-      // beforeEach is run before each of the tests
-      afterEach(() =>{
-          cy.log('after each hook is logged here')
-      })
-      //afterEach each is run after each of the tests have been executed
-      after(() => {
-          cy.log('the final after hook runs once.')
-      })
-      //after runs after all the the tests have been executed
-      //1st Test
-      it('renders the correct h1 text', () =>{
-          cy.get('h1').should('contain.text', 'Kitchen Sink')
-      })
-      //2nd Test
-      it('contains a h1', () =>{
-          cy.get('h1').should('exist')
-      })
-    })
+describe('Cypress Hooks', () => {
+  before(() =>{
+    cy.request('https://api.spacexdata.com/v3/missions').its('body').should('have.length', 10)
+  })
+  //before / beforeAll is run at the beginning of the test run
+  beforeEach(() =>{ 
+    cy.visit('/')
+  })
+  // beforeEach is run before each of the tests
+  afterEach(() =>{
+    cy.log('after each hook is logged here')
+  })
+  //afterEach each is run after each of the tests have been executed
+  after(() => {
+    cy.log('the final after hook runs once.')
+  })
+  //after runs after all the the tests have been executed
+  //1st Test
+  it('renders the correct h1 text', () =>{
+    cy.get('h1').should('contain.text', 'Kitchen Sink')
+  })
+  //2nd Test
+  it('contains a h1', () =>{
+    cy.get('h1').should('exist')
+  })
+})
